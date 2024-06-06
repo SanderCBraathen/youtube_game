131053498783dcf5c0390ae2db8f52f3165321e2
Sandify <101810166+Sandify@users.noreply.github.com>
2024-06-06 15:56:16 +0200

    1.3.0 release

--- lambo_buy.gd
+++ lambo_buy.gd
@@ -1,10 +1,9 @@
 extends Button
 
-var bought = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,14 +13,3 @@ func _process(delta):
 
 func _on_pressed():
 	self.hide()
-	bought = true
-
-
-func _on_control_over_1_4m():
-	if bought == false:
-		self.show()
-
-
-func _on_control_under_1_4m():
-	if bought == false:
-		self.hide()
