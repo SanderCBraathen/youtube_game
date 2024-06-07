9a75f15ddde3b2bdf9e3d51c8ecbabd04c7573a3
Sandify <101810166+Sandify@users.noreply.github.com>
2024-06-07 13:24:53 +0200

    fixed icons and ui

--- options.gd
+++ options.gd
@@ -1,9 +1,11 @@
 extends Control
 
+@onready var CM = $VBoxContainer/goofy_mode
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	await get_tree().create_timer(1).timeout
+	change_scene("res://main_menu.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,13 +13,8 @@ func _process(delta):
 	pass
 
 
-func _on_start_button_pressed():
-	change_scene("res://start_options.tscn")
-
+func _on_goofy_mode_toggled(toggled_on):
+	CM.add_theme_color_override("font_color", Color(0,1,0))
 
 func change_scene(target: String) -> void:
 	get_tree().change_scene_to_file(target)
-
-
-func _on_options_button_pressed():
-	change_scene("res://options.tscn")
